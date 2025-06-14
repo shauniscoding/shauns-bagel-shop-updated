b991d3fa43ae3109e364a95bb8762c81a17b8498
Shaun Thomas <shaunn.thomas@calbaptist.edu>
2025-06-14 12:41:54 -0700

    now scrolls up when changing pages mid scroll

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import Menu from "./pages/Menu/Menu";
 import Locations from "./pages/Locations/Locations";
 import Catering from "./pages/Catering/Catering";
 import About from "./pages/About/About";
+import Scrollup from "./Scrollup";
+
 
 function App() {
   return (
     <div>
+      <Scrollup />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
@@ -18,5 +21,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
