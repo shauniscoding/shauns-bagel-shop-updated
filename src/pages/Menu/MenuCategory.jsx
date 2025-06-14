4c6088b710c2bfa4206a612555fa371602a21a66
Shaun Thomas <shaunn.thomas@calbaptist.edu>
2025-06-14 12:33:17 -0700

    changed some text color to look nicer

--- src/pages/Menu/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory.jsx
@@ -2,35 +2,55 @@ import "./Menu.css";
 
 const MenuCategory = ({ image, category, onClick }) => {
   return (
+  <div
+    className="menu-category"
+    style={{
+      backgroundImage: `url(${image})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      width: "14vw",
+      height: "4vw",
+      flexShrink: 0,
+      borderRadius: "1vw",
+      position: "relative",
+      overflow: "hidden",
+    }}
+    onClick={onClick}
+  >
+    {/* Overlay */}
     <div
-      className="menu-category"
       style={{
-        backgroundImage: `url(${image})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        width: "14vw",
-        height: "4vw",
-        flexShrink: 0,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundColor: "rgba(0, 0, 0, 0.11)", // semi-transparent black overlay
         borderRadius: "1vw",
-        position: "relative",
+        pointerEvents: "none",
+        zIndex: 1,
       }}
-      onClick={onClick}
+    />
+
+    {/* Text */}
+    <h1
+      style={{
+        position: "relative",
+        bottom: "-0.6vw",
+        right: "-1vw",
+        fontSize: "1.4vw",
+        fontWeight: "bold",
+        color: "#f9f5eb", // white text for better contrast
+        zIndex: 2,
+        textShadow: "0 0 5px rgba(0,0,0,0.7)", // subtle shadow for readability
+      }}
+      className="menu-category-text"
     >
-      <h1
-        style={{
-          position: "relative",
-          bottom: "-0.6vw",
-          right: "-1vw",
-          fontSize: "1.4vw",
-          fontWeight: "bold",
-          color: "black",
-        }}
-        className="menu-category-text"
-      >
-        {category}
-      </h1>
-    </div>
-  );
+      {category}
+    </h1>
+  </div>
+);
+
 };
 
 export default MenuCategory;
